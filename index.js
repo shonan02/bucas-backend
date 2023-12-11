0ecf66a52bb057fbafc212697e09b9fa542b4677
shonan02 <66680460+shonan02@users.noreply.github.com>
2023-12-11 14:19:33 +0000

    Complete resolvers

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
+
 const { ApolloServer } = require('@apollo/server');
 const{ startStandaloneServer } = require('@apollo/server/standalone');
-require('dotenv').config();
-const mongoose = require('mongoose');
 
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const { GraphQLError } = require('graphql');
 const MONGODB_URI= process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log('connected to: ', MONGODB_URI);
     }).catch((err) => {
-        console.err(err.message);
+        console.log(err.message);
     })
 
 
@@ -30,7 +33,7 @@ const typeDefs = `
     }
     
     type Mutation {
-        createUser(
+        register(
             username: String!
             email: String!
             password: String!
@@ -41,19 +44,89 @@ const typeDefs = `
         ): Token
     }`
 
+
+const bcrpyt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const resolvers = {
     Query: {
-        me: () => 
+        me: (root, args, context) => {
+            return context.currentUser;
+        }
+    },
+    Mutation: {
+        register: async (root, args) => {
+
+            const passwordHash = await bcrpyt.hash(args.password, 10);
+            const user = new User({
+                username: args.username,
+                email: args.email,
+                password: passwordHash
+            })
+
+        return await user.save()
+            .catch(err => {
+                throw new GraphQLError('Creating user failed', {
+                    extensions:{
+                        code: 'BAD_USER_INPUT',
+                        err
+                    }
+                })
+            })
+        },
+        login: async (root, args) => {
+            const user = await User.findOne({ username: args.username});
+
+            if(!user) {
+                throw new GraphQLError('User not found', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT'
+                    }
+                })
+            }
+            
+            //Check whether the given password is valid
+            const isValid = await bcrpyt.compare(args.password, user.password);
+
+            if(!isValid) {
+                throw new GraphQLError('Incorrect password', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT'
+                    }
+                })
+            }
+
+            //Sign in the user and return a web token
+            const userForToken = {
+                username: user.username,
+                email: user.email,
+                id: user._id
+            }
+
+            //Create token with jsonwebtoken library
+            return({ value: jwt.sign(userForToken, process.env.SECRET)});
+        }
     }
 }
 
-const server = ApolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
+//
 startStandaloneServer(server, {
     listen: {port: 4000 },
+    context: async ({ req, res}) => {
+        const auth = req ? req.headers.authorization : null;
+        if(auth && auth.startsWith('Bearer ')) {
+            const decodedToken = jwt.verify(
+                auth.substring(7), process.env.SECRET
+            )
+            const currentUser = await User.findById(decodedToken.id);
+            return { currentUser }
+        }
+    }
 }).then(({ url }) => {
     console.log(`Server ready at ${url}`)
 })
